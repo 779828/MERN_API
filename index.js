2efb4c05a79b608f542ce800c53f6386c2f0d46b
Shashank <88761790+779828@users.noreply.github.com>
2024-11-26 10:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://memoriesmern-eight.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
